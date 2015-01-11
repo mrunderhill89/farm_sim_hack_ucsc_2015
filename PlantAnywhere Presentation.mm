8f34161d56dffe62db8fdc9eb1f1c092dd9e7f4a
Kevin Cameron <mrunderhill89@gmail.com>
2015-01-11 13:16:08 -0800

    Wrote the PowerPoint presentation. Probably last commit before code lock.

--- PlantAnywhere Presentation.mm
+++ PlantAnywhere Presentation.mm
@@ -1,10 +1,59 @@
-<map version="0.9.0">
+<map version="freeplane 1.2.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="PlantAnywhere Presentation" ID="ID_1447837362" CREATED="1421000456905" MODIFIED="1421000465526">
-<hook NAME="MapStyle" max_node_width="600"/>
+<node TEXT="PlantAnywhere Presentation" ID="ID_1447837362" CREATED="1421000456905" MODIFIED="1421000465526"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
 <node TEXT="Introduction" POSITION="right" ID="ID_600684916" CREATED="1421001590338" MODIFIED="1421001612753">
 <node TEXT="Hello, everyone" ID="ID_1122864843" CREATED="1421001613371" MODIFIED="1421001618920"/>
-<node TEXT="Great to be here after two days of hacking." ID="ID_1808038028" CREATED="1421001619172" MODIFIED="1421001628442"/>
+<node TEXT="Great to be standing here after two days of hacking." ID="ID_1808038028" CREATED="1421001619172" MODIFIED="1421007948586">
+<node TEXT="Great to be standing at all" ID="ID_316412934" CREATED="1421007949762" MODIFIED="1421007954027"/>
+</node>
 <node TEXT="Little awkward representing my entire &quot;team.&quot;" ID="ID_1579009802" CREATED="1421001631613" MODIFIED="1421001643739">
 <node TEXT="Looks can be deceiving" ID="ID_1999895600" CREATED="1421001644630" MODIFIED="1421001649091"/>
 <node TEXT="Wasn&apos;t -just- me sitting in the corner typing away." ID="ID_1048662984" CREATED="1421001682232" MODIFIED="1421001910756"/>
@@ -19,6 +68,7 @@
 <node TEXT="Outline data" ID="ID_1523108573" CREATED="1421001873069" MODIFIED="1421001888603"/>
 </node>
 <node TEXT="I&apos;m sure there are more" ID="ID_771640590" CREATED="1421001890823" MODIFIED="1421001894477"/>
+<node TEXT="Thank you to all involved" ID="ID_624419747" CREATED="1421007957020" MODIFIED="1421007971650"/>
 </node>
 </node>
 </node>
@@ -27,8 +77,8 @@
 <node TEXT="Based on SimFarm." ID="ID_1733998405" CREATED="1421001974293" MODIFIED="1421001978035"/>
 <node TEXT="Aims to recreate a small garden or hydroponic farm,&#xa;like what CityBlooms is doing." ID="ID_305514936" CREATED="1421001979245" MODIFIED="1421002017688"/>
 <node TEXT="Goal is" ID="ID_1116787547" CREATED="1421002019087" MODIFIED="1421002022750">
-<node ID="ID_748159924" CREATED="1421002105964" MODIFIED="1421002112848">
-<richcontent TYPE="NODE">
+<node ID="ID_748159924" CREATED="1421002105964" MODIFIED="1421002112848"><richcontent TYPE="NODE">
+
 <html>
   <head>
     
@@ -41,8 +91,8 @@
 </html>
 </richcontent>
 </node>
-<node ID="ID_1657475393" CREATED="1421002112852" MODIFIED="1421002112855">
-<richcontent TYPE="NODE">
+<node ID="ID_1657475393" CREATED="1421002112852" MODIFIED="1421002112855"><richcontent TYPE="NODE">
+
 <html>
   <head>
     
@@ -64,8 +114,8 @@
 </node>
 <node TEXT="instant updates" ID="ID_1498780868" CREATED="1421002330061" MODIFIED="1421002336754">
 <node TEXT="since it&apos;s a web application" ID="ID_751173214" CREATED="1421002337910" MODIFIED="1421002352946"/>
-<node ID="ID_1737002601" CREATED="1421002355052" MODIFIED="1421002417469">
-<richcontent TYPE="NODE">
+<node ID="ID_1737002601" CREATED="1421002355052" MODIFIED="1421002417469"><richcontent TYPE="NODE">
+
 <html>
   <head>
     
@@ -78,8 +128,8 @@
 </html>
 </richcontent>
 </node>
-<node ID="ID_1076485267" CREATED="1421002417473" MODIFIED="1421002417476">
-<richcontent TYPE="NODE">
+<node ID="ID_1076485267" CREATED="1421002417473" MODIFIED="1421002417476"><richcontent TYPE="NODE">
+
 <html>
   <head>
     
