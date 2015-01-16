ee2ec0b3e661d01c8f79c0eb46aa3d2bbec11676
Kevin <kevin@kevin-Gateway-Viper-C.(none)>
2015-01-15 17:21:36 -0800

    Played around with using behavior trees for the menu controls. It seems they're a bit too complicated for what I want, so I'll keep using EventStreams for now, though I'm keeping the BT code in case it comes in handy for something else.

--- js/controllers/behavior_tree.js
+++ js/controllers/behavior_tree.js
@@ -1,188 +1,185 @@
 define(['bacon','underscore'],function(Bacon,_){
     function BehaviorTree(run){
-        _.extend(this, {
-            parent: null,
-            children : [],
-            streams : {
-                child: new Bacon.Bus(),
-                results: new Bacon.Bus(),
-                run: new Bacon.Bus()
-            },
-            name: name || "node"
-        });
-        var bound_run = run.bind(this);
-        this.streams.run.debounce(2).onValue(bound_run);
-        return this;
-    };
-    _.extend(BehaviorTree, {
-        prototype: _.extend(BehaviorTree.prototype,{
-            run: function(params){this.streams.run.push(params);},
-            run_child: function(i, params, callback){
-                var child = this.children[i];
-                if (callback){
-                    child.onResult(callback);
-                }
-                child.run(params);
-                this.streams.child.push({
-                    child:child,
-                    i:i
-                });
-                return child;
-            },
-            result: function(value){this.streams.results.push(value);},
-            onResult: function(f){
-                this.streams.results.onValue(f);
-                return this;
-            },
-            child: function(run){
-                var child = new BehaviorTree(run);
-                child.parent = this;
-                this.children.push(child);
-                return child;
-            },
-            leaf: function(run){
-                this.child(run);
-                return this;
-            },
-            log: function(name){
-                this.streams.run.onValue(function(){
-                    console.log(name+".start:"+arguments);
-                });
-                this.streams.child.onValue(function(p){
-                    console.log(name+".child:"+p.i);
-                });
-                this.onResult(function(f){
-                    console.log(name+".result:"+f);
-                });
-                return this;
+        this.streams = {
+            results : new Bacon.Bus(),
+            run : new Bacon.Bus(),
+            prime : new Bacon.Bus()
+        }
+        if (run){
+            this.streams.run.onValue(function(){
+                run.apply(this,arguments);
+            }.bind(this));
+        }
+        this.streams.prime.onValue(function(args){
+            if (args.i < this.children.length){
+                var child = this.children[args.i];
+                child.onResult(args.callback).run.apply(child, args.arguments);
+            } else {
+                console.warn("Trying to prime a child that does not exist:"+args.i);
             }
-        }),
-        sequence: function(params, index){
-            var i = index || 0;
-            if (i >= this.children.length){
+        }.bind(this));
+        this.children = [];
+        this.parent = null;
+    }
+    function _sequence(index, result){
+        if (result === false){
+            this.result(false);
+        } else if (index+1 >= this.children.length){
+            this.result(true);
+        } else {
+            this.prime(index+1, _sequence.bind(this,index+1));
+        }
+        return Bacon.noMore;
+    }
+    function _select(index, result){
+        if (result === true){
+            this.result(true);
+        } else if (index+1 >= this.children.length){
+            this.result(false);
+        } else {
+            this.prime(index+1, _select.bind(this,index+1));
+        }
+        return Bacon.noMore;
+    }
+    function _para_sequence(){
+        console.log("Para Sequence");
+        var combined = _.reduce(this.children, function(stream, child, i){
+            return stream.combine(child.streams.result, function(rest, next){
+                if (rest && !next){
+                    this.result(false);
+                } else if (i+1 >= this.children.length){
+                    this.result(true);
+                }
+                return rest && next;
+            });
+        }, Bacon.once(true));
+        _.each(this.children, function(child){
+            child.run();
+        });
+    }
+    function _repeatUntil(value, result){
+        if (this.children.length < 1 ){
+            this.result(false);
+        } else {
+            if (result === value){
                 this.result(true);
             } else {
-                this.run_child(i,params,function(result){
-                    if (result == false){
-                        this.result(false);
-                    } else {
-                        BehaviorTree.sequence.bind(this)(params, i+1);
-                    }
-                    return Bacon.noMore;
-                }.bind(this));
-            }
-        },
-        select: function(params, index){
-            var i = index || 0;
-            if (i >= this.children.length){
-                this.result(false);
-            } else {
-                    this.run_child(i,params,function(result){
-                    if (result == true){
-                        this.result(true);
-                    } else {
-                        BehaviorTree.select.bind(this)(params, i+1);
-                    }
-                    return Bacon.noMore;
-                }.bind(this));
-            }
-        },
-        para_sequence: function(params){
-            var count = this.children.length;
-            var keys = _.map(this.children, function(child){
-                return child.streams.results.onValue(function(result,id){
-                    if (result){
-                        count--;
-                        if (count <= 0){
-                            this.result(true);
-                        }
-                    } else {
-                        _.each(keys, function(unsub){
-                            unsub();
-                        });
-                        this.result(false);
-                    }
-                    return Bacon.noMore;
-                }.bind(this))
-            }.bind(this))
-            _.each(this.children, function(child){child.run(params)});
-        },
-        para_select: function(params){
-            var count = this.children.length;
-            var keys = _.map(this.children, function(child){
-                return child.streams.results.onValue(function(result,id){
-                    if (!result){
-                        count--;
-                        if (count <= 0){
-                            this.result(false);
-                        }
-                    } else {
-                        _.each(keys, function(unsub){
-                            unsub();
-                        });
-                        this.result(true);
-                    }
-                    return Bacon.noMore;
-                }.bind(this))
-            }.bind(this))
-            _.each(this.children, function(child){child.run(params)});
-        },
-        check: function(test){
-            return function(){
-                var result = _.isFunction(test)? test(arguments):test;
-                this.result(result);
-            }
-        },
-        constant: function(result){
-            return function(params){
-                if (this.children.length > 0){
-                    this.run_child(0, params, function(){
-                        this.result(result);
-                    }.bind(this));
-                }
-            }
-        },
-        wait_for: function(stream, test){
-            return function(){
-                stream.onValue(function(params){
-                    var result = _.isFunction(test)? test(params):test;
-                    this.result(result);
-                    return Bacon.noMore;
-                }.bind(this));
-            }
-        },
-        run_lock: function(times){
-            return function(params){
-                this.runs = this.runs || 0;
-                if (this.runs <= times && this.children.length > 0){
-                    console.log(this.runs+"+");
-                    this.runs++;
-                    this.run_child(0,params, function(result){
-                        this.runs = Math.max(this.runs-1,0);
-                        console.log(this.runs+"-");
-                        this.result(result);
-                    }.bind(this));
-                } else {
-                    console.warn("run_lock is over limit:"+this.runs);
-                }
-            }
-        },
-        repeat_until: function(stop){
-            return function(params){
-                var finalResult = this.result;
-                if (this.children.length > 0){
-                    this.run_child(0,params,function(result){
-                        console.log(result);
-                        if (result == stop){
-                            finalResult(true);
-                            return Bacon.noMore;
-                        } else {
-                            this.run_child(0,params);
-                        }
-                    }.bind(this));
-                }
+                this.prime(0, _repeatUntil.bind(this,value));
             }
         }
-    });
+        return Bacon.noMore;
+    }
+    function _map(f){
+        if (this.children.length > 0){
+            this.prime(0, function(result){
+                if (_.isFunction(f)){
+                    this.result(f(result));
+                } else {
+                    this.result(f);
+                }
+                return Bacon.noMore;
+            }.bind(this));
+        } else {
+            if (_.isFunction(f)){
+                this.result(false);
+            } else {
+                this.result(f);
+            }
+        }
+    }
+    function _lock(t){
+        this.times = this.times || 0;
+        if (this.times < t){
+            this.times++;
+            this.prime(0, function(r){
+                this.times--;
+            }.bind(this));
+        } else {
+            console.warn("Lock overrun:"+this.times);
+        }
+    }
+    var types = {
+        sequence: function(){
+            return _sequence.bind(this,-1,true);
+        },
+        select: function(){
+            return _select.bind(this,-1,false);
+        },
+        para_sequence: function(){
+            return _para_sequence.bind(this);
+        },
+        para_select: function(){
+        },
+        repeatUntil: function(value){
+            return _repeatUntil.bind(this,value,undefined);
+        },
+        map: function(f){
+            return _map.bind(this,f);
+        },
+        lock: function(times){
+            return _lock.bind(this, times);
+        }
+    }
+    _.extend(BehaviorTree, 
+         _.reduce(types, function(memo, fun, key){
+            memo[key] = function(){
+                var node = new BehaviorTree();
+                var args = arguments;
+                node.run = fun.apply(node,args);
+                return node;
+            }.bind(this);
+            return memo;
+        }, {}),
+        {
+        prototype: _.extend(
+            BehaviorTree.prototype, 
+            {
+                run: function(){
+                    this.streams.run.push(arguments);
+                    return this;
+                },
+                onResult: function(callback){
+                    this.streams.results.onValue(callback);
+                    return this;
+                },
+                result: function(value){
+                    this.streams.results.push(value);
+                    return this;
+                },
+                prime: function(index, callback, args){
+                    this.streams.prime.push({i:index, callback:callback, arguments:args});
+                    return this;
+                },
+                child: function(node){
+                    this.children.push(node);
+                    node.parent = this;
+                    return this;
+                },
+                log: function(name){
+                    this.onResult(function(r){
+                        console.log(name+": returning "+r);
+                    });
+                    this.streams.run.onValue(function(){
+                        console.log(name+": running");
+                    });
+                    this.streams.prime.onValue(function(args){
+                        console.log(name+": priming "+args.i);
+                    });
+                    return this;
+                }
+            }
+        ),
+        every: function(delay, value){
+            var node = new BehaviorTree();
+            node.streams.results.plug(Bacon.interval(delay,value));
+            return node;
+        },
+        map_stream: function(stream, fun){
+            var node = new BehaviorTree();
+            node.streams.results.plug(stream.map(fun));
+            return node;
+        }
+    })
+    console.log(BehaviorTree.prototype);
     return BehaviorTree;
 });
--- js/controllers/menu_tree.js
+++ js/controllers/menu_tree.js
@@ -1,5 +1,39 @@
 define(['underscore', 'controllers/behavior_tree'],function(_, BT){
     function MenuControl(params){
+        var root = BT.lock(1);
+        root.child(
+            BT.repeatUntil(false).child(
+                BT.map(true).child(
+                    BT.select().log("Select").child(
+                        //Add Plant
+                        BT.sequence().child(
+                            BT.map_stream(params.menu_click, function(name){
+                                return (name === "add_plant");
+                            })
+                        ).child(
+                            BT.repeatUntil(true).child(
+                                BT.para_sequence().log("Para-Sequence").child(
+                                    BT.sequence().log("Add Plant").child(
+                                        BT.map_stream(params.soil_click, function(e){
+                                            return true;
+                                        })
+                                    ).child(
+                                        new BT(function(){
+                                            this.result(true);
+                                        })
+                                    )
+                                ).child(
+                                    BT.map_stream(params.menu_click, function(name){
+                                        return (name === "add_plant");
+                                    }).log("Exit Menu")
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        );
+        return root;
     }
     return MenuControl;
 })
